5111832b135363b32b4d69c231f8e525b4bfe5e8
James Ward <james@jamesward.com>
2021-02-05 15:27:14 -0700

    to testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
-  kotlin("jvm") version "1.4.21" apply false
-  kotlin("plugin.spring") version "1.4.21" apply false
+  kotlin("jvm") version "1.4.30" apply false
+  kotlin("plugin.spring") version "1.4.30" apply false
 }
 
 allprojects {
   repositories {
     mavenCentral()
-    jcenter()
-    google()
     maven(url = "https://packages.confluent.io/maven")
     maven(url = "https://repository.mulesoft.org/nexus/content/repositories/public/")
-
   }
 }
